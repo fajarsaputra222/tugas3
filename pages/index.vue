0db1404527c1dcd93f855ce23d86dc42f6b95523
fajarsaputra222 <120171484+fajarsaputra222@users.noreply.github.com>
2023-09-10 05:58:29 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Task List</h1>
+    <TaskList />
+  </div>
+</template>
+
+<script>
+import TaskList from '~/components/TaskList.vue';
+
+export default {
+  components: {
+    TaskList
+  }
+};
+</script>
